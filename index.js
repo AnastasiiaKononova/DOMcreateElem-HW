936f04b718c7e63df8d39f5cc74616679e3a05de
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2024-09-13 13:58:53 +0300

    Add 1-st task resolve

---
+++ index.js
@@ -0,0 +1,7 @@
+//1
+
+const section = document.createElement('section');
+section.setAttribute('id', 'root');
+section.textContent = 'Это произвольный текст внутри секции';
+section.style.color = 'green';
+document.body.append(section);
